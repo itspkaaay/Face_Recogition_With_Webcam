8420ab41d19789c58ecffb705858bd543ceb4b64
Parikshit Sachdeva <65595301+itspkaaay@users.noreply.github.com>
2020-05-21 19:37:54 +0530

    Update facedatasetcreation.py

--- facedatasetcreation.py
+++ facedatasetcreation.py
@@ -18,12 +18,12 @@ while True:
     for face in faces:
         (x,y,w,h)=face
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
-    frame_gray_cropped= frame_gray[y-offset:y+h+offset,x-offset:x+w+offset]
-    face_to_save=cv2.resize(frame_gray_cropped,(100,100))
-    ctr=ctr+1
-    if ctr%10==0:                        #Storing every 10th frame from webcam video Stream
-        face_data.append(face_to_save)
-        print(len(face_data)," saved")
+        frame_gray_cropped= frame_gray[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_to_save=cv2.resize(frame_gray_cropped,(100,100))
+        ctr=ctr+1
+        if ctr%10==0:
+            face_data.append(face_to_save)
+            print(len(face_data)," saved")
     cv2.imshow('frame rgb',frame)
     cv2.imshow('frame cropped gray',frame_gray_cropped)
     
@@ -35,8 +35,8 @@ while True:
 face_data= np.asarray(face_data)
 face_data= face_data.reshape(face_data.shape[0],-1)
 print(face_data.shape)
-
-np.save(filepath+name,".npy")
+name=filepath+name+".npy"
+np.save(name,face_data)
 print('Data Collected')
 
 cap.release()
